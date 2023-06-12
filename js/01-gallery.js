90a7a861621dcb3bc5b68b37f4f40c5847daaa24
Ruslan0920 <rus_boy2002@ukr.net>
2023-06-12 16:02:30 +0300

    upd 01-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,52 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const fullGallery = document.querySelector('.gallery');
-console.log(fullGallery);
+// console.log(fullGallery);
 // 
 const createFullGallery = galleryItems.map(({preview, original, description}) =>
     `<li class = "gallery__item">
     <a class = "gallery__link" href="${original}">
-    <img class = "gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+    <img class = "gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}">
     </a>
     </li>`
-
 )
 fullGallery.insertAdjacentHTML('beforeend', createFullGallery.join(" "));
 fullGallery.addEventListener('click', onClick);
 
 function onClick(event) {
     event.preventDefault();
-    // console.log(event.target);
+
     if (!event.target.classList.contains('gallery__image')) {
         return;
     }
-    // console.log(event.target.currentSrc);
+    // console.log(event.target.dataset.source);
+    // console.log(event.target.alt);
     
     const instance = basicLightbox.create(
-    `<img  class = "gallery__link" src="${event.target.currentSrc}" alt="${createFullGallery.description}">`
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`
 )
-
     instance.show();
-    // event. preventDefault()
+
+    
 }
-
-// console.log(createFullGallery);
-
-// import * as basicLightbox from 'basiclightbox'
-
-
-
-
-
-
-// import * as basicLightbox from 'basiclightbox'
-
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `)
-
-// instance.show()
-
