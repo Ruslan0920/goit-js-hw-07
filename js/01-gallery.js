201a3a88e680c7a9a9f00b3baaa30cc5fa3c9fa9
Ruslan0920 <rus_boy2002@ukr.net>
2023-06-14 15:59:32 +0300

    add upd 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
-
 const fullGallery = document.querySelector('.gallery');
-// console.log(fullGallery);
-// 
-const createFullGallery = galleryItems.map(({preview, original, description}) =>
+
+const createFullGallery = galleryItems.map(({ preview, original, description }) =>
     `<li class = "gallery__item">
     <a class = "gallery__link" href="${original}">
     <img class = "gallery__image"
@@ -15,36 +12,28 @@ const createFullGallery = galleryItems.map(({preview, original, description}) =>
         alt="${description}">
     </a>
     </li>`
-)
+);
+
 fullGallery.insertAdjacentHTML('beforeend', createFullGallery.join(" "));
 fullGallery.addEventListener('click', onClick);
 
 function onClick(event) {
     event.preventDefault();
-
-    if (!event.target.classList.contains('gallery__image')) {
+    const newTarget = event.target;
+    console.log(newTarget);
+    if (!newTarget.classList.contains('gallery__image')) {
         return;
     }
-    // console.log(event.target.dataset.source);
-    // console.log(event.target.alt);
-    
     
     const instance = basicLightbox.create(
-    `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`
+    `<img src="${newTarget.dataset.source}" alt="${newTarget.alt}">`
 )
     instance.show();
-    console.log(instance);
 
     window.addEventListener('keydown', pressEsc);
 
     function pressEsc(event) {
-        console.log(event.keyCode);
         if (event.keyCode === 27)
-            console.log(event);
-            // event.remove()
-        // closeModal();
+            instance.close()
     }
-// function closeModal() {
-//         instance.remove()
-//     }
 }
